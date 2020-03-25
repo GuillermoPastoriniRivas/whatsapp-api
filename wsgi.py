982052e4d66739057b8f9f7c578b7749826af125
GuillermoPastoriniRivas <guillepastorini@hotmail.com>
2020-03-25 18:38:20 -0300

    Changes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from application import create_app
 from config import *
+from decouple import config as config_decouple
+
+enviroment = config['development']
+if config_decouple('PRODUCTION', default=False):
+    enviroment = config['production']
 
 app = create_app(enviroment)
